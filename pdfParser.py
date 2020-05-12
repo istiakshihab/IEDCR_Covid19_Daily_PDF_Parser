fb13e50beb28c9bd8f79a59eae9857ce9860bc3d
istiakshihab <i.h.shihab@live.com>
2020-05-12 19:10:10 +0600

    Here we go again

--- pdfParser.py
+++ pdfParser.py
@@ -11,6 +11,7 @@ link= soup.find_all('a', href=True)
 fileUrl= url+ link[5].get("href")
 file = link[5].get("href").split(".")
 fileName = file[0]
+print("Downloading "+fileName)
 fileLoc = str(fileName+".pdf")
 
 urllib.request.urlretrieve(fileUrl,fileLoc)
