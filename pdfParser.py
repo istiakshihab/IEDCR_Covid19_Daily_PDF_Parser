9cf7d0855578db76d747d72e533fffe2a709e50c
istiakshihab <i.h.shihab@live.com>
2020-05-15 15:58:27 +0600

    Fixes

--- pdfParser.py
+++ pdfParser.py
@@ -7,12 +7,15 @@ url = "https://www.iedcr.gov.bd/website/images/files/nCoV/"
 resp = urllib.request.urlopen(url+"?C=M;O=D")
 soup = BeautifulSoup(resp, from_encoding=resp.info().get_param('charset'),features="lxml")
 
-link= soup.find_all('a', href=True)
-fileUrl= url+ link[5].get("href")
-file = link[5].get("href").split(".")
-fileName = file[0]
-fileLoc = str(fileName+".pdf")
-print("Downloading as "+fileLoc)
+for link in soup.find_all('a', href=True):
+    x= link.get("href").split("_")
+    if x[0]=="Case":
+        fileUrl= url+ link.get("href")
+        file = link.get("href").split(".")
+        fileName = file[0]
+        fileLoc = str(fileName+".pdf")
+        print("Downloading as "+fileLoc)
+        break
 
 urllib.request.urlretrieve(fileUrl,fileLoc)
 
